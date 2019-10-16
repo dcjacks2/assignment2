2fe5bee9764da790aa6b8cbfa705f56161add9ec
dcjacks2 <bockielou@hotmail.com>
2019-10-15 22:18:54 -0500

    added strings and ints for the student class

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    private int ID;
+    private int standing;
+    private String address;
+    private String fName;
+    private String lName;
+    private String email;
+    private static int scount;
+}
