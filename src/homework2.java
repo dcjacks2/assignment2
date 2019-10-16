5588559eb87e17a45733750feb39c5ca61b9dcae
dcjacks2 <bockielou@hotmail.com>
2019-10-15 23:18:14 -0500

    initializing student array and scanner for asking how many students will be in the array

--- src/homework2.java
+++ src/homework2.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class homework2 {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N;
+        System.out.println("How many students Data will be entered?: ");
+        N = input.nextInt();
+        Student[] s=new Student[N];
+
 
     }
 }
