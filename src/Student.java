8d869b45e9d3c24b35a545ba072dbb9bd84973cf
dcjacks2 <bockielou@hotmail.com>
2019-10-19 20:54:28 -0500

    created increment count to count number of students

--- src/Student.java
+++ src/Student.java
@@ -8,7 +8,7 @@ public class Student { //initializers
     private String fName;
     private String lName;
     private String email;
-    private static int scount;
+    private static int count;
 
     public void setValues(int i1, String s7, int i3, String s1, String s2, String s3, String s4, String s5, String s6 ){
         ID = i1;
@@ -21,16 +21,21 @@ public class Student { //initializers
         lName = s5;
         email = s6;
     }
-    public void getValues(){
+    public void displayInfo(){
+
+        System.out.println("First Name: "+ fName);
+        System.out.println("Last Name: "+ lName);
         System.out.println("ID: " + ID);
         System.out.println("Standing: "+ standing);
         System.out.println("Zip: "+ zip);
         System.out.println("State: "+ state);
         System.out.println("City: "+ city);
         System.out.println("Address: "+ address);
-        System.out.println("First Name: "+ fName);
-        System.out.println("Last Name: "+ lName);
         System.out.println("Email: "+ email);
+    }
+    public static int incrementCount(){
+        count = count+1;
+        return(count);
     }
 
 
